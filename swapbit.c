e48db23d17b238c3252f055bd66883006b15a142
KamilaBanu <30333760+KamilaBanu@users.noreply.github.com>
2017-10-10 16:38:41 +0530

    Create swapbit.c

---
+++ swapbit.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int i = 65;
+    int k = 120;
+    printf(" %d %d", i, k);
+    i = i ^ k;
+    k = i ^ k;
+    i = i ^ k;
+    printf("%d %d ", i, k);
+    return 0;
+}
